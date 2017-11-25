4c7e849a2e36fb6d0841c71134e88a64b809392e
rudhrabalankari <mail2rudrasimha@gmail.com>
2017-11-25 17:46:53 -0500

    displaying the image item selected

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,18 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
 
     recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'Test Desc', 'https://cdn.pixabay.com/photo/2017/06/21/22/42/recipe-2428926_960_720.jpg'),
-        new Recipe('Another Test Recipe', 'Test Desc', 'https://cdn.pixabay.com/photo/2017/06/21/22/42/recipe-2428926_960_720.jpg')
+        new Recipe('Omlet',
+            'Egg Omlet with Cheese',
+            'http://img.taste.com.au/TP6Cmork/taste/2016/11/chinese-omelette-31745-1.jpeg',
+            [new Ingredient('Eggs', 2), new Ingredient('Cheese', 1, ), new Ingredient('Onions', 1)]),
+        new Recipe('French Fries', 'Salted Potato Fries',
+            'https://cdn.potatopro.com/cdn/farfuture/JHyuloN9w_v4NBD7TkfmaZfWN6i49VrlsfspygR2J5Y/mtime:1495749506/sites/default/files/pictures/french-fries-1200.jpg',
+            [new Ingredient('Potatoes', 3), new Ingredient('Salt', 1)])
     ];
 
 
